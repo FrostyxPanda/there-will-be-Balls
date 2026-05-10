74315d527f776da5694b02aacc3d67731d3f54c3
FrostyxPanda <bluepandarifi@gmail.com>
2026-05-09 23:13:59 -0500

    Mobile test2

--- assets/themes/Beach/cloud_spawner.gd
+++ assets/themes/Beach/cloud_spawner.gd
@@ -7,14 +7,10 @@ extends Node
 @onready var front_layer = $"../DynamicForeground/CloudsFront"
 
 var timer = 0.0
-
 var arena_left = 108
 var arena_right = 974
-
-# ☁️ visible cloud band
-var arena_top = 250
-var arena_bottom = 650
-
+var arena_top = 260
+var arena_bottom = 1000
 
 func _process(delta):
 
@@ -26,77 +22,52 @@ func _process(delta):
 
 		timer = randf_range(2.0, 5.0)
 
-
 func spawn_cloud():
 
-	print("Cloud spawned")
-
 	var cloud = cloud_scene.instantiate()
-
-	# 🌎 arena bounds
+	
 	cloud.arena_left = arena_left - 600
 	cloud.arena_right = arena_right + 600
+	
 
-	# ☁️ random layer
-	var layers = [
-		back_layer,
-		mid_layer,
-		front_layer
-	]
 
+	# random layer
+	var layers = [back_layer, mid_layer, front_layer]
 	var chosen = layers.pick_random()
 
 	chosen.add_child(cloud)
 
-	# ☁️ spawn height
-	var spawn_y = randf_range(
-		arena_top,
-		arena_bottom
-	)
+	# spawn position
+	var spawn_y = randf_range(arena_top, arena_bottom)
 
-	# 🌬️ all clouds move right → left
-	cloud.direction = -1
+	if cloud.direction == -1:
+	# spawn on right
+		cloud.position = Vector2(arena_right, spawn_y)
+	else:
+	# spawn on left
+		cloud.position = Vector2(arena_left, spawn_y)
+		cloud.direction = [-1, 1].pick_random()
+		
+	cloud.speed = randf_range(150, 250)
 
-	# ☁️ spawn offscreen right
-	cloud.position = Vector2(
-		arena_right + 200,
-		spawn_y
-	)
-
-	# ☁️ default speed
-	cloud.speed = randf_range(250, 500)
-
-	# ☁️ layer depth settings
+	# layer depth settings
 	match chosen.name:
 
 		"CloudsBack":
-
-			cloud.speed = randf_range(10, 30)
-
+			cloud.speed = randf_range(10, 60)
 			cloud.scale *= 1.4
-
-			cloud.modulate.a = 0.35
-
-			cloud.target_alpha = randf_range(0.2, 0.35)
-
+			cloud.modulate.a = 0.3
+			cloud.target_alpha = randf_range(0.15, 0.35)
 
 		"CloudsMid":
-
-			cloud.speed = randf_range(25, 50)
-
-			cloud.scale *= 1.0
-
+			cloud.speed = randf_range(25, 80)
+			cloud.scale *= 0.9
 			cloud.modulate.a = 0.5
-
-			cloud.target_alpha = randf_range(0.35, 0.5)
-
+			cloud.target_alpha = randf_range(0.3, 0.5)
 
 		"CloudsFront":
-
-			cloud.speed = randf_range(40, 70)
-
-			cloud.scale *= 0.8
-
-			cloud.modulate.a = 0.75
-
+			cloud.speed = randf_range(35, 70)
+			cloud.scale *= 0.7
+			cloud.modulate.a = 0.8
 			cloud.target_alpha = randf_range(0.5, 0.8)
+		
