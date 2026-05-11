f9b9cc6e28fa82d538534f955479ed863752d21b
FrostyxPanda <bluepandarifi@gmail.com>
2026-05-11 13:10:14 -0500

    trying to fix clouds on mobile

--- assets/themes/Beach/cloud.gd
+++ assets/themes/Beach/cloud.gd
@@ -4,8 +4,10 @@ extends Node2D
 
 @onready var sprite = $Sprite2D
 
+
 # ☁️ Cloud texture pool
 var cloud_textures = [
+
 	preload("res://assets/themes/beach/clouds/cloud-2.png"),
 	preload("res://assets/themes/beach/clouds/cloud-5.png"),
 	preload("res://assets/themes/beach/clouds/cloud-7.png"),
@@ -16,30 +18,32 @@ var cloud_textures = [
 	preload("res://assets/themes/beach/clouds/cloud-27.png"),
 	preload("res://assets/themes/beach/clouds/cloud-31.png"),
 	preload("res://assets/themes/beach/clouds/cloud-43.png"),
+
 ]
+
+
 var direction = -1
+
 var arena_left = 0
 var arena_right = 1080
 var target_alpha = 0.6
 
+
+
 func _ready():
 
-	# 🎨 Random cloud image
+	# ☁️ random cloud image
 	sprite.texture = cloud_textures.pick_random()
 
-	# 🌫️ Slight variation
+	# ☁️ slight variation
 	scale *= randf_range(0.8, 1.4)
 
-	modulate.a = 0.0
 
-	target_alpha = randf_range(0.3, 0.8)
 
 func _process(delta):
 
 	# ☁️ movement
 	position.x += speed * direction * delta
-
-	modulate.a = 1.0
 
 	# 🗑️ cleanup
 	if global_position.x < arena_left - 400:
